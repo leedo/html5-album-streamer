8703e1d13983a54148cede0d6a526990159de884
leedo <lee@laylward.com>
2010-07-20 14:25:36 -0500

    uses soundmanager2, falls back to flash

--- js/streamer.js
+++ js/streamer.js
@@ -1,8 +1,9 @@
 var Streamer = Class.create({
+
   initialize: function (element) {
     this.playlist_url = element.getAttribute("rel");
     this.element = element;
-    this.volume = 50;
+    this.volume = 100;
     this.songs = [];
     this.activeSong;
     this.progressTimer;
@@ -15,6 +16,7 @@ var Streamer = Class.create({
       [ ".previous", this.previous ],
       [ ".volume", this.updateVolume ]
     ];
+
     this.element.observe("click", function (event) {
       this.clickHandlers.each(function (handler) {
         var elem = event.findElement(handler[0]);
@@ -24,34 +26,37 @@ var Streamer = Class.create({
         }
       }.bind(this));
     }.bind(this));
+
   },
+
   changeSong: function (elem) {
     this.stop();
     this.updateProgress();
     var a = elem.down('a');
-    var mime = Streamer.getMime(a.href);
-    if (Streamer.canPlayType(mime)) {
+
+    if (this.activeSong)
+      soundManager.destroySound(this.activeSong.sID);
+
+    if (soundManager.canPlayLink(a)) {
       elem.addClassName("active");
-      this.activeSong = document.createElement('audio');
-      this.activeSong.volume = this.volume / 100;
-      this.activeSong.src = a.href;
-      this.activeSong.observe("ended", this.next.bind(this));
-      this.activeSong.observe("error", function (e) {
-        this.displayError("Your browser can not play "+mime+" files");
-      }.bind(this));
+      this.activeSong = soundManager.createSound(a.href, a.href);
+      soundManager.setVolume(this.activeSong.sID, this.volume);
+      this.onfinish = this.next.bind(this);
       this.element.down(".title").innerHTML = a.innerHTML;
       this.play();
     }
     else {
-      this.displayError("Your browser can not play "+mime+" files");
+      this.displayError("Your browser can not play this audio file");
     }
   },
+
   togglePlay: function (elem) {
     if (!this.activeSong || (this.activeSong && this.activeSong.paused))
       this.play();
     else
       this.pause();
   },
+
   next: function () {
     var active = this.element.down(".active");
     if (!active)
@@ -63,43 +68,55 @@ var Streamer = Class.create({
       this.stop();
     }
   },
+
   previous: function () {
     var active = this.element.down(".active");
     if (active && active.previous())
       this.changeSong(active.previous());
   },
+
   play: function () {
     if (!this.activeSong)
       this.changeSong(this.element.down(".streamersong"));
+
     if (this.activeSong) {
-      this.activeSong.play();
+      console.log(this.activeSong.sID);
+      soundManager.play(this.activeSong.sID);
       this.element.down(".play").addClassName("pause");
       this.progressTimer = setInterval(this.updateProgress.bind(this), 500);
     }
   },
+
   pause: function () {
-    if (this.activeSong) this.activeSong.pause();
+    if (this.activeSong)
+      soundManager.pause(this.activeSong.sID);
+
     this.element.down(".play").removeClassName("pause");
     clearInterval(this.progressTimer);
   },
+
   stop: function () {
     if (this.activeSong) {
       this.element.down(".active").removeClassName("active");
       this.element.down(".title").innerHTML = "";
-      this.pause();
-      if (this.activeSong.currentTime) this.activeSong.currentTime = 0;
+      soundManager.stop(this.activeSong.sID);
+      soundManager.destroySound(this.activeSong.sID);
       this.activeSong = undefined;
       this.updateProgress();
     }
+
     clearInterval(this.progressTimer);
   },
+
   updateProgress: function () {
     var width = 0;
-    if (this.activeSong)
-      width = (this.activeSong.currentTime / this.activeSong.duration)
+    if (this.activeSong) {
+      width = (this.activeSong.position / this.activeSong.duration)
               * this.progressWidth();
+    }
     this.element.down(".progress").setStyle({width: width+"px"});
   },
+
   refreshPlaylist: function () {
     new Ajax.Request("/playlist", {
       method: "get",
@@ -113,6 +130,7 @@ var Streamer = Class.create({
       }.bind(this)
     })
   },
+
   parsePlaylist: function (xml) {
     var urls = xml.match(/<location>.*?<\/location>/g);
     var titles = xml.match(/<title>.*?<\/title>/g);
@@ -127,11 +145,13 @@ var Streamer = Class.create({
       this.songs.push({url: url[1], title: (i+1)+". "+title[1]});
     }
   },
+
   progressWidth: function () {
     if (!this._progressWidth)
       this._progressWidth = this.element.down(".bar").getWidth() - this.element.down(".controls").getWidth();
     return this._progressWidth;
   },
+
   initPlayer: function () {
     this.element.innerHTML = "";
     this.element.insert({top: '<div class="bar"><div class="progress"></div><div class="controls"><span class="previous"></span><span class="stop"></span><span class="play"></span><span class="next"></span></div><div class="title"></div><div class="volume"><div class="volume_bg"></div></div></div>'});
@@ -142,50 +162,27 @@ var Streamer = Class.create({
     this.songs.each(function (song) {
       list.insert(Streamer.songTemplate.evaluate(song));
     });
-    if (!Streamer.canPlayAudio())
+
+    soundManager.onerror = function() {
       this.displayError("The &lt;audio&gt; element is not supported by your browser");
+    };
   },
+
   displayError: function (err) {
     this.element.down(".title").innerHTML = "<span class=\"error\">"+err+" :-(</span>";
   },
+
   updateVolume: function (elem, event) {
     var vol = event.offsetX;
     elem.down(".volume_bg").setStyle({width: vol+"px"});
-    this.volume = vol / 17;
+    this.volume = (vol / 17) * 100;
     if (this.activeSong)
-      this.activeSong.volume = this.volume;
+      soundManager.setVolume(this.activeSong.sID, this.volume);
   }
 });
 
 Object.extend(Streamer, {
-  songTemplate: new Template("<li class=\"streamersong\"><a href=\"#{url}\">#{title}</a></li>"),
-  mimeMap: {
-    mp3: "audio/mpeg",
-    m4a: "audio/mp4",
-    mp4: "audio/mp4",
-    ogg: "audio/ogg",
-    oga: "audio/ogg",
-    wav: "audio/x-wav",
-    flac: "audio/flac"
-  },
-  canPlayType: function (mime) {
-    var elem = document.createElement('audio');
-    if (elem.canPlayType) {
-      var r = elem.canPlayType(mime);
-      return r == 'maybe' || r == 'probably';
-    }
-    return false;
-  },
-  canPlayAudio: function () {
-    var elem = document.createElement('audio');
-    return !!elem.canPlayType;
-  },
-  getMime: function (href) {
-    var ext = href.match(/.*\.(.+?)(?:$|\?)/);
-    if (ext) {
-      return Streamer.mimeMap[ext[1]];
-    }
-  },
+  songTemplate: new Template("<li class=\"streamersong\"><a href=\"#{url}\">#{title}</a></li>")
 });
 
 document.observe("dom:loaded", function () {
